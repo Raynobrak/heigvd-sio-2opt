ef9e6340e500299dab7ff6e7fd4772a9fc12db25
Raynobrak <lcs.charbonnier@gmail.com>
2024-11-29 10:42:34 +0100

    initial commit

---
+++ src/main/java/ch/heig/sio/lab2/groupX/Gui.java
@@ -0,0 +1,27 @@
+package ch.heig.sio.lab2.groupX;
+
+import ch.heig.sio.lab2.display.HeuristicComboItem;
+import ch.heig.sio.lab2.display.ObservableTspConstructiveHeuristic;
+import ch.heig.sio.lab2.display.ObservableTspImprovementHeuristic;
+import ch.heig.sio.lab2.display.TspSolverGui;
+import ch.heig.sio.lab2.tsp.RandomTour;
+import com.formdev.flatlaf.FlatLightLaf;
+
+public final class Gui {
+  public static void main(String[] args) {
+    ObservableTspConstructiveHeuristic[] constructiveHeuristics = {
+        new HeuristicComboItem.Constructive("Random tour", new RandomTour()),
+        // Add the constructive heuristics
+    };
+
+    ObservableTspImprovementHeuristic[] improvementHeuristics = {
+        // Add the new improvement heuristic
+    };
+
+    // May not work on all platforms, comment out if necessary
+    System.setProperty("sun.java2d.opengl", "true");
+    FlatLightLaf.setup();
+
+    new TspSolverGui(1400, 800, "TSP solver", constructiveHeuristics, improvementHeuristics);
+  }
+}
